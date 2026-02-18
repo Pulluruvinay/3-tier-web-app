1735d57dcddd653a18e273905f2c46ec30a9424f
vinay <vinaykumarreddy14082003@gmail.com>
2026-02-18 12:50:33 +0530

    trigger pipeline1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "vinay-terraform-state"
+    bucket         = "vinay-terraform-state2"
     key            = "three-tier-app/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
