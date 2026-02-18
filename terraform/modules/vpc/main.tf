796d402b4ea66f29a5e420348a349e41ea148ab2
vinay <vinaykumarreddy14082003@gmail.com>
2026-02-18 14:15:14 +0530

    trigger pipeline3

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   tags = { Name = "three-tier-vpc" }
@@ -12,10 +14,21 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
 }
 
 resource "aws_subnet" "private" {
-  count      = 2
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
 }
+
