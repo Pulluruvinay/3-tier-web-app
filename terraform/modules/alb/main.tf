b0122221d45b9a2633a95ad42149ffdeee969174
vinay <vinaykumarreddy14082003@gmail.com>
2026-02-18 16:52:13 +0530

    git rm -r --cached terraform/.terraform

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,14 +1,47 @@
+########################################
+# ALB SECURITY GROUP
+########################################
+
+resource "aws_security_group" "alb" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+########################################
+# APPLICATION LOAD BALANCER
+########################################
+
 resource "aws_lb" "main" {
   load_balancer_type = "application"
   subnets            = var.public_subnets
+  security_groups    = [aws_security_group.alb.id]
 }
+
+########################################
+# TARGET GROUP (FARGATE REQUIRES IP)
+########################################
 
 resource "aws_lb_target_group" "main" {
   name        = "three-tier-tg"
   port        = 5000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
-  target_type = "ip" 
+  target_type = "ip"
 
   health_check {
     path                = "/"
@@ -19,8 +52,15 @@ resource "aws_lb_target_group" "main" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+########################################
+# LISTENER
+########################################
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
@@ -36,4 +76,3 @@ resource "aws_lb_listener" "http" {
     create_before_destroy = true
   }
 }
-
