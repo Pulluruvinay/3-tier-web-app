d659fc8d7ab32f8e2c6e93de68e0bcacfc770fcf
vinay <vinaykumarreddy14082003@gmail.com>
2026-02-18 14:45:50 +0530

    Fix target group lifecycle dependency

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -25,9 +25,15 @@ resource "aws_lb_target_group" "main" {
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
