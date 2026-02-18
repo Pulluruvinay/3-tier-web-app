1f2cfd873e7e7f90363bf2e2ff2fa3dde7e09f7d
vinay <vinaykumarreddy14082003@gmail.com>
2026-02-18 14:37:00 +0530

    Fix target group for fargate

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -4,11 +4,23 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_target_group" "main" {
-  name     = "three-tier-tg"
-  port     = 5000
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "three-tier-tg"
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip" 
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
+
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
